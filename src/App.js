6c10411ddbab76c071622d5bc1314370458e77f3
aniketkumar <kaniket123456789@gmail.com>
2024-05-16 00:06:08 +0530

    added the tree functionality

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ import 'reactflow/dist/style.css';
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: '3', position: { x: 0, y: 100 }, data: { label: '3' } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' },
+{ id: 'e1-3', source: '1', target: '3' }
+];
  
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
